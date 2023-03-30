5facfcdad674ab360ee532b0037011b682f9bd15
AbubakarKadzo <husnabakar01@gnail.com>
2023-03-30 09:27:38 +0300

    JAVASCRIPT

---
+++ java.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <body>
+            <script>
+                function myFunction() {
+                    Var txt;
+                    if (confirm("Confirm Purchase") {
+                        txt = "Order confirmed";
+                    } else {
+                        txt = "Cancel Order";
+                    }
+                    })
+                }
+            </script>
+        </body>
+    </head>
+</html>
